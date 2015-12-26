8609d0c18cb1be68705b4c8d1ae40eb4b738c79a
timcartwright <tim@websitesbytim.com>
2015-12-26 11:33:08 +0530

    Create Location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,4 +3,17 @@ class LocationsController < ApplicationController
     @game = Game.find(params[:game_id])
     @location = Location.new
   end
+
+  def create
+    location = Location.new(location_params)
+    game_id = params[:game_id]
+    location.game_id = game_id
+    location.save!
+    redirect_to game_path(game_id)
+  end
+
+private
+  def location_params
+    params.require(:location).permit(:lat, :long, :name, :description)
+  end
 end
